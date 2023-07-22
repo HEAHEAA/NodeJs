b98d364f7508109416435da32412e59ba572121e
Jihee Kim <kimjihee1113@gmail.com>
2023-07-22 17:13:32 +0900

    get요청 보내기 & html 파일 요청 & 부트스트랩을 이용한 페이지 적용

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+//첫 서버를 열수있는 정해진 코드
 const express = require('express');
 const app = express();
 
@@ -5,7 +7,31 @@ app.listen(8080, function(){
     console.log('listening on 8080')
 });
 
+
 //1. 누군가가 /pet 으로 방문하면 pet 관련된 안내문을 띄우기 (GET)
 app.get('/pet', function(req,res){
     res.send('펫용품 쇼핑할 수 있는 페이지 입니다.');
 });
+
+//2. 숙제 : /beauty url로 접속하면 안내문("뷰티용품페이지임") 띄우기 
+app.get('/beauty', function(req,res){
+    res.send("뷰티용품페이지임");
+});
+
+//html get 파일 날리기
+app.get('/' , function(req,res){
+    res.sendFile(__dirname + '/Page/index.html');
+});
+
+//write get 페이지
+app.get('/write', function(req,res){
+    res.sendFile(__dirname + '/Page/write.html');
+});
+
+
+
+
+
+
+
+
